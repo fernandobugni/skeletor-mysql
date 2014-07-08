f2819172fa199a980f1b6955a8f62a762a9f3f5e
jlbali <juan.lucas.bali@gmail.com>
2014-07-08 15:35:31 -0300

    Cambios menores en app.js

--- app.js
+++ app.js
@@ -76,8 +76,6 @@ app.use(function(err, req, res, next) {
 });
 
 
-// Asegurarse que el Gulp este corriendo!!
-
 app.set('port', process.env.PORT || 3000); 
 app.listen(app.get('port'), function() { 
     console.log('Express server listening on port ' + app.get('port')); 
