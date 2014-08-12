143dd43f74e37692f81bc0d6695f8ec6e91ff316
Gaturron <fernando.bugni@gmail.com>
2014-08-12 16:19:05 -0300

    prueba MySQL

--- app.js
+++ app.js
@@ -5,8 +5,20 @@ var logger = require('morgan');
 var cookieParser = require('cookie-parser');
 var bodyParser = require('body-parser');
 
-// DB.
-var schemas = require('./db/schemas').schemas;
+// DB: prueba
+var mysql      = require('mysql');
+var connection = mysql.createConnection({
+  host     : 'localhost',
+  user     : 'me',
+  password : 'secret'
+});
+//connection.connect();
+//connection.query('SELECT 1 + 1 AS solution', function(err, rows, fields) {
+//  if (err) throw err;
+//  console.log('The solution is: ', rows[0].solution);
+//});
+//connection.end();
+
 // Middleware
 var schemasMiddleware = require('./schemasMiddleware');
 // Rutas
